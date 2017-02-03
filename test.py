30ed9534a5fdf0bb60793dd40b50edc920697c7d
pavlokach <kachmar@ucu.edu.ua>
2017-02-03 11:48:05 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,18 @@
 import random
 import os
 
-def get_questions(file_name):
+def get_questions(fname):
     # Gets needed quetion
-    questions = []
-    #with  open(os.getcwd() + "/mysite/database/" + file_name, encoding="utf-8") as f:
-    with  open(os.getcwd() + "/database/" + file_name, encoding="utf-8") as f:
-        #f = f.readline().decode('utf-8')
-        for line in f:
-            questions.append(line.strip().split(';'))
-    return random.choice(questions)
+    import sqlite3
+    name = fname.split('.')[0]
+    conn = sqlite3.connect(os.getcwd() + '/database/' + 'db.sqlite')
+    cur = conn.cursor()
+    cur.execute(('''SELECT * FROM {0} ORDER BY RANDOM() LIMIT 1''').format(name.strip()))
+    return cur.fetchall()
+
 
 def get_module_questions():
+    # Gets needed quetion
     import random
     question_lst = ['quotes.txt', 'logic.txt', 'logic1.txt',
                     'sets.txt', 'functions.txt', 'ratio.txt',
@@ -55,28 +56,6 @@ def check_position(n):
 
 def find_coords(n):
     # Finds cells coordinates
-    '''
-    if n < 10 and n > 0:
-        x = 95 + 80 * (n - 1) + 40
-        y = -40
-    if n > 10 and n < 20:
-        x = 870
-        y = -95 + -(n - 11) * 80 - 40
-    if n > 20 and n < 30:
-        y = -870
-        x = 95 + (29 - n) * 80 + 40
-    if n > 30 and n < 40:
-        x = 40
-        y = -95 + (n - 39) * 80 - 40
-    if n == 0:
-        x, y = 40, -40
-    if n == 10:
-        x, y = 870, 40
-    if n == 20:
-        x, y = 870, -870
-    if n == 30:
-        x, y = 40, -870
-    return x, y'''
     data_dict = {0: (25, 47), 1: (25, 109), 2: (25, 169), 3: (25, 226), 4: (25, 285), 5: (25, 341), 6: (25, 398), 7: (25, 453), 8: (25, 509), 9: (25, 565), 10: (25, 625), 11: (87, 625), 12: (147, 625), 13: (204, 625), 14: (263, 625), 15: (319, 625), 16: (376, 625), 17: (431, 625), 18: (487, 625), 19: (543, 625), 20: (597, 625), 21: (597, 565), 22: (597, 509), 23: (597, 453), 24: (597, 398), 25: (597, 341), 26: (597, 285), 27: (597, 226), 28: (597, 169), 29: (597, 109), 30: (597, 47), 31: (543, 47), 32: (487, 47), 33: (431, 47), 34: (376, 47), 35: (319, 47), 36: (263, 47), 37: (204, 47), 38: (147, 47), 39: (87, 47), 40: (25, 47)}
     return data_dict[n][1], data_dict[n][0]
 
@@ -87,6 +66,7 @@ def dice_generator():
 
 
 class Player:
+    # Main class in game
     location = 0
     laps = 0
 
@@ -99,7 +79,6 @@ class Player:
         if self.location >= 40:
             self.location -= 40
             self.laps += 1
-        #print("Self.location", self.location)
         x2, y2 = find_coords(self.location)
         #print(x2, y2, "LOL")
         return x2, y2
